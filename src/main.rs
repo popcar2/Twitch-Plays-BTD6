aa8a928783b817cdf821ade4e27f8fb82c19c009
ahmed nassef <ahmoudy2000@gmail.com>
2022-05-22 22:58:04 +0200

    Add tower placement

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use twitch_irc::SecureTCPTransport;
 use twitch_irc::TwitchIRCClient;
 use twitch_irc::message::ServerMessage;
 
-use tokio::time::Duration;
+use tokio::time::{sleep, Duration};
 use tokio::{task, time};
 
 use std::collections::HashMap;
@@ -12,7 +12,12 @@ use std::collections::HashMap;
 use parking_lot::Mutex;
 use std::sync::Arc;
 
+use mouse_rs::{Mouse, types::keys::Keys};
+
 mod voting;
+
+const SCREEN_X: i32 = 1920;
+const SCREEN_Y: i32 = 1080;
 
 #[tokio::main]
 pub async fn main() {
@@ -58,6 +63,26 @@ pub async fn main() {
         }
     });
 
+    // This is code used for testing the mouse boundries. DO NOT RUN THIS IF YOU DON'T KNOW WHAT YOU'RE DOING.
+    // THIS FUNCTION WILL HIJACK YOUR MOUSE AND CLICK A TON OF PLACES. BE CAREFUL.
+    /*let mouse_derp = task::spawn(async move{
+        for i in 1..40{
+            for j in 1..24{
+                if i > 32{ continue; }
+                let mouse = Mouse::new();
+                let x_dist = (f64::from(SCREEN_X / 40 * i + 25) / 1.25).floor() as i32;
+                let y_dist = (f64::from(SCREEN_Y / 24 * j) / 1.25).floor() as i32;
+                mouse.move_to(x_dist, y_dist);
+                mouse.press(&Keys::LEFT).expect("Unable to press button");
+                mouse.release(&Keys::LEFT).expect("Unable to release button");
+                sleep(Duration::from_millis(20)).await;
+                //println!("x: {}, y: {}", SCREEN_X / 40 * i + 25, SCREEN_Y / 24 * j);
+                println!("Location: {}{}", "abcdefghijklmnopqrstuvwxyz".chars().nth(j as usize - 1).unwrap(), i);
+            }
+        }
+    });*/
+
     join_handle.await.unwrap();
     forever.await.unwrap();
+    //mouse_derp.await.unwrap();
 }
--- src/voting.rs
+++ src/voting.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 use mki::Keyboard;
-//use mouse_rs::{types::keys::Keys, Mouse};
+use mouse_rs::{Mouse, types::keys::Keys};
+
+use std::thread;
+use std::time::Duration;
 
 pub enum VotingPhase{
     Regular,
@@ -9,7 +12,8 @@ pub enum VotingPhase{
 
 // Validate this is a vote syntax
 pub fn validate_vote(message_text: &str, phase: &VotingPhase) -> bool{
-    if message_text.to_lowercase().starts_with("tower ") && matches!(phase, VotingPhase::Regular){
+    // Choose a tower (ex: tower bomb)
+    if matches!(phase, VotingPhase::Regular) && message_text.to_lowercase().starts_with("tower "){
         let second_word = message_text.split(" ").nth(1).unwrap().trim_end().to_lowercase();
         match second_word.as_str(){
             "hero" | "dart" | "boomerang" | "bomb" | "tack" | "ice"
@@ -20,16 +24,35 @@ pub fn validate_vote(message_text: &str, phase: &VotingPhase) -> bool{
             _ => {}
         }
     }
+    // Place a tower (ex: a28) from a-x 1-32 because these are usable locations in the play area
+    // Checks in order: is in placement phase, first letter between a and x, 2nd char is a number
+    else if matches!(phase, VotingPhase::Placement) && message_text.len() > 1 
+    && "abcdefghijklmnopqrstuvw".contains(message_text.to_lowercase().chars().next().unwrap())
+    && message_text.chars().nth(1).unwrap().is_numeric(){
+        // has 2 numbers (ex: a17). Can't go higher than 32.
+        if message_text.len() > 2{
+            if message_text.chars().nth(2).unwrap().is_numeric(){
+                let tile_num = String::from(message_text)[1..3].parse::<u8>().unwrap();
+                if tile_num > 0 && tile_num < 33{
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        
+        // only 1 number (ex: a7). This can't be 0 (ex: a0)
+        if message_text.chars().nth(1).unwrap() == '0'{
+            return false;
+        }
+        return true;
+    }
     return false;
 }
 
 pub fn add_vote(votes: &mut HashMap<String, String>, user_id: String, message_text: String){
-    let mut word_iter = message_text.split(" ");
-    let mut test = word_iter.next().unwrap().trim().to_lowercase();
-    test += " ";
-    test += word_iter.next().unwrap().trim().to_lowercase().as_str();
-    println!("{}", test);
-    votes.insert(user_id, test);
+    println!("{}", message_text.to_lowercase());
+    votes.insert(user_id, message_text.to_lowercase());
 
     /*for (key, value) in &*votes{
         println!("{} / {}", key, value);
@@ -58,19 +81,22 @@ pub fn collect_votes(votes: &mut HashMap<String, String>, phase: &mut VotingPhas
             highest_count = value;
         }
     }
-    println!("{} won at {} votes!", highest_vote, highest_count);
     votes.clear();
 
     if highest_count > 0{
+        println!("{} won at {} votes!", highest_vote, highest_count);
         activate_vote(highest_vote, phase);
+    }
+    else{
+        println!("Nobody voted, nothing happened...");
     }
 }
 
 fn activate_vote(final_vote: String, phase: &mut VotingPhase){
     let first_word = final_vote.split(" ").next().unwrap();
-    let second_word = final_vote.split(" ").nth(1).unwrap();
 
     if first_word == "tower"{
+        let second_word = final_vote.split(" ").nth(1).unwrap();
         if second_word == "hero"{
             Keyboard::U.click();
         }
@@ -142,5 +168,48 @@ fn activate_vote(final_vote: String, phase: &mut VotingPhase){
         }
         *phase = VotingPhase::Placement;
         println!("Entering placement phase...");
+    }
+
+    else if matches!(phase, VotingPhase::Placement){
+        // Adding 1 to row and column's defaults so it doesn't click the very edge of the screen and instead starts a little inwards.
+        let row: u32 = "abcdefghijklmnopqrstuvw".find(final_vote.chars().next().unwrap()).unwrap() as u32 + 1;
+        let column: u32;
+        
+        // TODO: Get resolution and scaling % and left padding
+        if final_vote.len() > 2{
+            column = String::from(final_vote)[1..3].parse::<u32>().unwrap();
+        }
+        else{
+            column = String::from(final_vote)[1..2].parse::<u32>().unwrap();
+        }
+        println!("{}", column);
+
+        let mouse = Mouse::new();
+        let x_dist = (f64::from(1920 / 40 * column + 25) / 1.25).floor() as i32;
+        let y_dist = (f64::from(1080 / 24 * row) / 1.25).floor() as i32;
+        mouse.move_to(x_dist, y_dist).expect("Unable to move mouse");
+
+        // Sometimes it would click before moving, so I'll move the mouse first then click after a small delay
+
+        // TODO: check if it got placed???
+        // Force cancel by pushing the red X in C33 if the tower failed to get placed properly
+        thread::spawn(|| {
+            let mouse = Mouse::new();
+
+            thread::sleep(Duration::from_millis(100));
+
+            mouse.press(&Keys::LEFT).expect("Unable to press button");
+            mouse.release(&Keys::LEFT).expect("Unable to release button");
+
+            thread::sleep(Duration::from_millis(100));
+            let x_dist = (f64::from(1920 / 40 * 33 + 25) / 1.25).floor() as i32;
+            let y_dist = (f64::from(1080 / 24 * 3) / 1.25).floor() as i32;
+            mouse.move_to(x_dist, y_dist).expect("Unable to move mouse");
+            mouse.press(&Keys::LEFT).expect("Unable to press button");
+            mouse.release(&Keys::LEFT).expect("Unable to release button");
+        });
+
+        *phase = VotingPhase::Regular;
+        println!("Entering regular phase...");
     }
 }
